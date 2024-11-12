137d1085e7286b2f84be8535918e45a9036aa44d
MickaelMasson <masson.mickael@me.com>
2024-11-12 10:15:28 +0100

    itinial commit

---
+++ eau14.py
@@ -0,0 +1,22 @@
+"""
+"""
+
+# Fonctions utilisées
+
+# Partie 1 : Gestion d'erreur
+
+# Partie 2 : Parsing
+
+# Partie 3 : Résolution
+
+# Partie 4 : Affichage
+
+"""
+
+
+XXhXXm pour faire cet exercice. 
+Principal difficulté: 
+- 
+Ce que j'ai appris : 
+- 
+"""
