fd81ed0ecb3bd6898012f97e9de6724f68dc1008
MickaelMasson <masson.mickael@me.com>
2024-11-19 11:15:47 +0100

    String dans sting

--- eau05.py
+++ eau05.py
@@ -1,61 +1,39 @@
 """String dans string"""
-
+import sys
 # Fonctions utilisées
-
-def comparer_arguments(liste_argument):
-    argument_a = liste_argument[0]
-    argument_b = liste_argument[1]
-    if (argument_a in argument_b) or (argument_b in argument_a):
-        reponse = True
+def compare_arguments(arguments):
+    first_argument = arguments[0]
+    second_argument = arguments[1]
+    if (first_argument in second_argument) or (second_argument in first_argument):
+        awnser = True
     else:
-        reponse = False
-    return reponse
-
+        awnser = False
+    return awnser
 
 # Partie 1 : Gestion d'erreur
-
-def check_input(arguments_input):
-    try:
-        if " " in arguments_input :
-            liste_argument = arguments_input.split()
-            argument_a = liste_argument[0]
-            argument_b = liste_argument[1]
-        else :
-            print("error")
-            exit()
-            
-    except ValueError:
-        print("error ValueError")
-    except UnboundLocalError:
-        print("error UnboundLocalError")
-
-    return ([argument_a, argument_b])
-
+def check_arguments(arguments_input):
+    if len(arguments) != 2 :
+        print("Error, vous devez saisir 2 arguments")
+        sys.exit()
 
 # Partie 2 : Parsing
-
-arguments = input()
-
+arguments = sys.argv[1:]
 
 # Partie 3 : Résolution
-
-if comparer_arguments(check_input(arguments)):
-    print("true")
-
-else :
-    print("false")
+check_arguments(arguments)
+is_corresponding = compare_arguments(arguments)
 
 # Partie 4 : Affichage
-
+print(is_corresponding)
 """
 
 
 1h pour faire cet exercice. 
 Principal difficulté: 
 - nommer les variables et les fonctions
-- j'ai du sorti une liste plutot que 2 arguments dans check_input() 
+- j'ai du sorti une liste plutot que 2 arguments dans check_arguments() 
   car j'avais une erreur comme quoi il me manquait l'argument b 
-  en entrée de comparer_arguments(), j'ai donc du avoir une liste à
+  en entrée de compare_arguments(), j'ai donc du avoir une liste à
   l'entrée puis la séparer a nouveau
 Ce que j'ai appris : 
 - le exit() pour arrter le script 
