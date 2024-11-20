3d8450e31d31460ef7aedb8c863db333c183aeac
MickaelMasson <masson.mickael@me.com>
2024-11-20 05:58:39 +0100

    Majuscule sur Deux / correction avec fonction partout

--- eau06.py
+++ eau06.py
@@ -1,35 +1,73 @@
 """Majuscule sur deux"""
+import sys
 
 # Fonctions utilisées
+def is_alpha(letter) :
+    if letter.isalpha() :
+        return True
+    else :
+        return False
+
+def get_alter_upper_lower(arguments) :
+    argument = arguments[0]
+    new_string = ""
+    last_letter_is_upper = False
+    for i in range(len(argument)) :
+        letter = argument[i]
+        if is_alpha(letter) :
+            if last_letter_is_upper :
+                new_string = new_string + letter.lower()
+                last_letter_is_upper = False
+            else :
+                new_string = new_string + letter.upper()
+                last_letter_is_upper = True
+        else :        
+            new_string = new_string + letter
+    
+    return new_string
 
 # Partie 1 : Gestion d'erreur
+def is_valid_number_arguments (arguments):
+    if len(arguments) != 1 :
+        print("Error, vous devez saisir une seule chaine de caractère")
+        return False
+    return True
 
+def is_digit_argument (arguments) :
+    argument = arguments[0]
+    if argument.isdigit():
+        print("Error, vous devez saisir une seule chaine de caractère avec a-z/A-Z")
+        return False
+    return True
+    
 # Partie 2 : Parsing
+def get_arguments() :
+    arguments = sys.argv[1:]
+    return arguments
 
 # Partie 3 : Résolution
+def get_new_string() :
+    if not is_valid_number_arguments(get_arguments()) :
+        return
+    if not is_digit_argument(get_arguments()) :
+        return
+    
+    return get_alter_upper_lower(get_arguments())
 
 # Partie 4 : Affichage
+def display() :
+    print(get_new_string())
 
-"""
+display()
 
-
-XXhXXm pour faire cet exercice. 
-Principal difficulté: 
-- 
-Ce que j'ai appris : 
-- 
-
-Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères. Seule les lettres (A-Z, a-z) sont prises en compte.
-
+"""Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères. 
+Seule les lettres (A-Z, a-z) sont prises en compte.
 
 Exemples d’utilisation :
 $> python exo.py “Hello world !”
 HeLlO wOrLd !
 
-
 $> python exo.py 42
 error
 
-Afficher error et quitter le programme en cas de problèmes d’arguments.
-
-"""
+Afficher error et quitter le programme en cas de problèmes d’arguments."""
