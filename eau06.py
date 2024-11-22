f89d7ad595480f53a86bb6dc35bdf5e31403148b
MickaelMasson <masson.mickael@me.com>
2024-11-22 05:45:31 +0100

    Majuscule sur deux / correction espace

--- eau06.py
+++ eau06.py
@@ -23,11 +23,10 @@ def get_alter_upper_lower(arguments) :
                 last_letter_is_upper = True
         else :        
             new_string = new_string + letter
-    
     return new_string
 
 # Partie 1 : Gestion d'erreur
-def is_valid_number_arguments (arguments):
+def is_valid_number_arguments (arguments) :
     if len(arguments) != 1 :
         print("Error, vous devez saisir une seule chaine de caractère")
         return False
@@ -35,7 +34,7 @@ def is_valid_number_arguments (arguments):
 
 def is_digit_argument (arguments) :
     argument = arguments[0]
-    if argument.isdigit():
+    if argument.isdigit() :
         print("Error, vous devez saisir une seule chaine de caractère avec a-z/A-Z")
         return False
     return True
@@ -46,19 +45,15 @@ def get_arguments() :
     return arguments
 
 # Partie 3 : Résolution
-def get_new_string() :
+def display_new_string() :
     if not is_valid_number_arguments(get_arguments()) :
         return
     if not is_digit_argument(get_arguments()) :
         return
-    
-    return get_alter_upper_lower(get_arguments())
+    print(get_alter_upper_lower(get_arguments()))
 
 # Partie 4 : Affichage
-def display() :
-    print(get_new_string())
-
-display()
+display_new_string()
 
 """Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères. 
 Seule les lettres (A-Z, a-z) sont prises en compte.
