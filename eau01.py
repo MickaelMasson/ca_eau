dd568ea0687a70a2b95f9e4162d8651647128d48
MickaelMasson <masson.mickael@me.com>
2024-11-27 14:40:50 +0100

    Combinaisons de 2 numbers

--- eau01.py
+++ eau01.py
@@ -1,26 +1,26 @@
 """Combinaisons de 2 numbers"""
 
 # Fonctions utilisées
-def diplay_combinations() :
+def get_combination() :
     combination_list = []
-
     for i in range(100) :
         i = str(i).zfill(2)
         for j in range(100) :
             j = str(j).zfill(2)
             if i < j :
                 combination_list.append(f"{i} {j}")
-    
-    print(", ".join(combination_list))
+    return combination_list
 
 # Partie 1 : Gestion d'erreur
 
 # Partie 2 : Parsing
 
 # Partie 3 : Résolution
-
+def display_combination() :
+    print(", ".join(get_combination()))
+    
 # Partie 4 : Affichage
-diplay_combinations()
+display_combination()
 
 """
 Créez un programme qui affiche toutes les différentes combinaisons de deux nombre entre 00 et 99 dans l’ordre croissant.
