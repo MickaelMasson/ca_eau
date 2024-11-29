720ab269ce54eaa8f212ed1f47b0764e17607ceb
MickaelMasson <masson.mickael@me.com>
2024-11-29 09:46:10 +0100

    Majuscule sur deux / suite correction Romain

--- eau06.py
+++ eau06.py
@@ -10,26 +10,23 @@ def is_alpha(character: str) -> bool :
 def get_alter_upper_lower(argument: str) -> str :
     new_string = ""
     last_character_is_upper = False
-    for i in range(len(argument)) :
-        character = argument[i]
-        if is_alpha(character) :
-            if last_character_is_upper :
-                new_string = new_string + character.lower()
-                last_character_is_upper = False
-            else :
-                new_string = new_string + character.upper()
-                last_character_is_upper = True
-
+    for character in argument :
+        if is_alpha(character) and last_character_is_upper :
+            new_string = new_string + character
+            last_character_is_upper = False
+        elif is_alpha(character) :
+            new_string = new_string + character.upper()
+            last_character_is_upper = True
         else :        
             new_string = new_string + character
     return new_string
 
 # Partie 1 : Gestion d'erreur
-def is_valid_arguments(arguments: list, number_of_argument: int) :
+def is_valid_arguments(arguments: list, number_of_argument: int) -> bool:
     if len(arguments) != number_of_argument :
         print("Error, vos arguments ne sont pas valide")
         return False
-    return arguments
+    return True
 
 def is_only_digit(string: str) -> bool:
     for character in string :
@@ -37,14 +34,14 @@ def is_only_digit(string: str) -> bool:
             return False
     print(f"Error, l'argument attendu ne peut pas etre un nombre")    
     return True
-    
+
 # Partie 2 : Parsing
 def get_arguments() -> list :
     arguments = sys.argv[1:]
     return arguments
 
 # Partie 3 : Résolution
-def display_new_string() :
+def display_alter_upper_lower() :
     arguments = get_arguments()
     number_of_argument_expected = 1
     if not is_valid_arguments(arguments, number_of_argument_expected) :
@@ -55,7 +52,7 @@ def display_new_string() :
     print(get_alter_upper_lower(argument))
 
 # Partie 4 : Affichage
-display_new_string()
+display_alter_upper_lower()
 
 """Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères. 
 Seule les lettres (A-Z, a-z) sont prises en compte.
