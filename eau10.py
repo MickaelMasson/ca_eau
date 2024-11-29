2287df3be60ff449d558ae5d992fb50c7ca6ead1
MickaelMasson <masson.mickael@me.com>
2024-11-29 13:37:12 +0100

    Index wanted / suite correction Romain

--- eau10.py
+++ eau10.py
@@ -2,48 +2,46 @@
 import sys
 
 # Fonctions utilisées
-def keyword(arguments) :
-    keyword = arguments[-1]
-    return keyword
-
-def find_keyword(arguments) :
-    for i in range(len(arguments) - 1) :
-        if arguments[i] == keyword(arguments) :
-            index = i
-            return index
-    index = -1
-    return index
+def get_index_wanted(search_base:list , index: str) -> str:
+    for i in range(len(search_base)) :
+        if search_base[i] == index :
+            index_wanted = str(i)
+            return index_wanted
+    index_wanted = "Error, -1"
+    return index_wanted
 
 # Partie 1 : Gestion d'erreur
-def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 2 :
-        print("error")
+def is_valid_arguments(arguments: list, number_of_argument: int) -> bool:
+    if len(arguments) < number_of_argument :
+        print("Error, le nombe d'arguments n'est pas valide")
         return False
     return True
 
 # Partie 2 : Parsing
-def get_arguments():
+def get_arguments()-> list :
     arguments = sys.argv[1:]
     return arguments
 
 # Partie 3 : Résolution
-def display() :
-    if not is_valid_number_of_arguments(get_arguments()) :
+def display_index_wanted() :
+    arguments = get_arguments()
+    min_number_of_argument_expected = 2
+    if not is_valid_arguments(arguments, min_number_of_argument_expected) :
         return
-    print(find_keyword(get_arguments()))
+    index = arguments[-1]
+    search_base = arguments[:-1]
+    print(get_index_wanted(search_base, index))
 
 # Partie 4 : Affichage
-display()
+display_index_wanted()
 """
 Créez un programme qui affiche le premier index d’un élément recherché dans un tableau. 
 Le tableau est constitué de tous les arguments sauf le dernier. 
 L’élément recherché est le dernier argument. Afficher -1 si l’élément n’est pas trouvé.
 
-
 Exemples d’utilisation :
 $> python exo.py Ceci est le monde qui contient Charlie un homme sympa Charlie
 6
-
 
 $> python exo.py test test test
 0
@@ -52,7 +50,6 @@ $> python exo.py test boom
 -1
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
-
 
 XXhXXm pour faire cet exercice. 
 Principal difficulté: 
